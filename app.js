07750f12e61b96ed0c4220cddc9052763a0e5eb3
d0bsidian <101651861+d0bsidian@users.noreply.github.com>
2022-08-19 15:16:35 +0200

    --

--- app.js
+++ app.js
@@ -3,21 +3,30 @@ const guessingGame () => {
     let guess;
     let numberToGuess = Math.ceil(Math.random() * 100);
     
-}
-
-
-
-
-
 
 const guessButtons = document.querySelectorAll('.btn btn-outline-light');
-
-
-
-
-
 guessButtons.forEach(button => {
     button.addEventListener('.click', () => {
 
     })
 });
+
+updateScreen() => 
+
+
+do {
+    guess = Number(updateScreen);
+    if(guess === null) {
+        break;
+    } else if (guess === numberToGuess) {
+        updateScreen("You guessed it in" + totalGuesses + (totalGuesses === 1 ? "try." : "tries."));
+        break;
+        } else if (number < numberToGuess) {
+            updateScreen = "Sorry, your guess was too low. Try again:";
+        } else {
+            updateScreen = "Sorry, your guess was too high. Try again:";
+        }
+        totalGuesses++;
+    }
+    while(true);
+};
