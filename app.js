947a513394e551fd048a8e93c6e1c2d3fc452292
d0bsidian <101651861+d0bsidian@users.noreply.github.com>
2022-08-19 14:55:54 +0200

    beginning with js

---
+++ app.js
@@ -0,0 +1,23 @@
+const guessingGame () => {
+    let totalGuesses = 1;
+    let guess;
+    let numberToGuess = Math.ceil(Math.random() * 100);
+    
+}
+
+
+
+
+
+
+const guessButtons = document.querySelectorAll('.btn btn-outline-light');
+
+
+
+
+
+guessButtons.forEach(button => {
+    button.addEventListener('.click', () => {
+
+    })
+});
