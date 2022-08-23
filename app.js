187efd6053d5e65c550d46fd7f68d67cbd13d7a2
d0bsidian <101651861+d0bsidian@users.noreply.github.com>
2022-08-23 15:14:52 +0200

    need to rewrite code

--- app.js
+++ app.js
@@ -1,32 +1,39 @@
-const guessingGame () => {
-    let totalGuesses = 1;
-    let guess;
-    let numberToGuess = Math.ceil(Math.random() * 100);
-    
+function guessGame() {
+    const buttons = document.querySelectorAll('btn btn-outline-light');
 
-const guessButtons = document.querySelectorAll('.btn btn-outline-light');
-guessButtons.forEach(button => {
-    button.addEventListener('.click', () => {
+const btn = document.querySelectorAll('.button');
+button.forEach(button => button.addEventListener('click'));
 
-    })
-});
+let guess = "";
+let totalGuesses = 1;
+let numberToGuess = Math.ceil(Math.random() * 10);
 
-updateScreen() => 
-
-
-do {
-    guess = Number(updateScreen);
-    if(guess === null) {
-        break;
-    } else if (guess === numberToGuess) {
-        updateScreen("You guessed it in" + totalGuesses + (totalGuesses === 1 ? "try." : "tries."));
-        break;
-        } else if (number < numberToGuess) {
-            updateScreen = "Sorry, your guess was too low. Try again:";
-        } else {
-            updateScreen = "Sorry, your guess was too high. Try again:";
-        }
-        totalGuesses++;
+class guessingGame {
+    constructor(guessingTextTextElement) {
+        this.guessingTextTextElement = guessingTextTextElement;
     }
-    while(true);
-};
+
+ guess() {
+    let guessing;
+    const current = this.guessingTextTextElement;
+    if(current === null) {
+        break;
+    } else if (guess === numberToGuess) { 
+        alert ("You guessed it in " + totalGuesses + (totalGuesses === 1 ? "try." : " tries."));
+        break;
+    } else if (guess < numberToGuess) {
+    promptMessage = "Sorry, your guess was too low. Try again:";
+    } else { 
+    promptMessage = "Sorry, your guess was too high. Try again:";
+    }
+    totalGuesses++;
+    }
+
+    getGuess(text) {
+        this.guessingTextTextElement = this.numberToGuess(this.guess);
+        if(this.guess != null) {
+            this.guessingTextTextElement.innerText;
+        }
+    }
+ }
+}
