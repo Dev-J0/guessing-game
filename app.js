6a8fb74d4f8ede3b057ac81870196f9008e3aec9
d0bsidian <101651861+d0bsidian@users.noreply.github.com>
2022-08-23 15:33:03 +0200

    added stuff

--- app.js
+++ app.js
@@ -1,39 +1,6 @@
-function guessGame() {
-    const buttons = document.querySelectorAll('btn btn-outline-light');
+let numberToGuess = Math.ceil(Math.random() * 100) + 1;
+const turnNumber = 1;
+const guesses = document.querySelectorAll('.btn btn-outline-light');
+const lowOrHigh = document.querySelector('.lowOrHigh');
 
-const btn = document.querySelectorAll('.button');
-button.forEach(button => button.addEventListener('click'));
-
-let guess = "";
-let totalGuesses = 1;
-let numberToGuess = Math.ceil(Math.random() * 10);
-
-class guessingGame {
-    constructor(guessingTextTextElement) {
-        this.guessingTextTextElement = guessingTextTextElement;
-    }
-
- guess() {
-    let guessing;
-    const current = this.guessingTextTextElement;
-    if(current === null) {
-        break;
-    } else if (guess === numberToGuess) { 
-        alert ("You guessed it in " + totalGuesses + (totalGuesses === 1 ? "try." : " tries."));
-        break;
-    } else if (guess < numberToGuess) {
-    promptMessage = "Sorry, your guess was too low. Try again:";
-    } else { 
-    promptMessage = "Sorry, your guess was too high. Try again:";
-    }
-    totalGuesses++;
-    }
-
-    getGuess(text) {
-        this.guessingTextTextElement = this.numberToGuess(this.guess);
-        if(this.guess != null) {
-            this.guessingTextTextElement.innerText;
-        }
-    }
- }
-}
+const guess
